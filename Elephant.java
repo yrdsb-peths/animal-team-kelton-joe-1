cb52ef205fc6b3ac2cf9083211dfe1d4d0abcda4
Joe <53659792+joez-zh@users.noreply.github.com>
2025-05-16 14:44:54 -0400

    increased max elephant speed

--- Elephant.java
+++ Elephant.java
@@ -56,7 +56,7 @@ public class Elephant extends Actor
     
     // base elephant speed of 4
     int speed = 4;
-    int maxSpeed = 15;
+    int maxSpeed = 18;
     
     // speed change timer cooldown
     int cooldownTime = 15;
